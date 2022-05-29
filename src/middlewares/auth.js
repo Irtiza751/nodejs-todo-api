a0c977913bfd2b9d1ea79bd57f8d95bfecf1a560
Irtiza751 <muhammad.irtiza751@gmail.com>
2022-05-29 13:48:45 +0500

    Update .env file & Add auth.js file to midllewares

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+async function auth(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        if(!token) {
+            return new Error('You must need to authorize.');
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded._id;
+        next();
+    } catch (error) {
+        res.status(401).json({msg: error.message});
+    }
+}
+
+module.exports = auth;
