d53207bf1da70f83dd4e62d2bdbcd65f2f50473d
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2022-06-01 11:58:17 +0500

    Update Task.js
    Made a simple task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -4,3 +4,10 @@ const mongoose = require('mongoose');
 * @TODO
 * Make simple task data modle and export.
 */
+const TaskSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  body: {type: String, required: false},
+  isDone: {type: Boolean, default: false}
+}, {timestamps: true});
+
+module.exports = mongoose.model('Task', TaskSchema);
