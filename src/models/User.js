44ac0c1c5c4af94bad10907e267979b212b301f8
Irtiza751 <muhammad.irtiza751@gmail.com>
2022-07-08 22:35:03 +0500

    Remove login/logout bugs we were facing.
    - by add a check if the user modifies the password field or not.
    - we were runing encryption every time when pre save ran.

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Task = require('./Task');
+const { AES, enc } = require('crypto-js');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, trim: true },
@@ -26,8 +26,12 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function (next) {
     const user = this;
     user.token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
-    const hashPassword = await bcrypt.hash(user.password, 8);
-    user.password = hashPassword;
+    // only change the hash after the user update his/her password.
+    if (user.isModified('password')) {
+        //const hashPassword = await bcrypt.hash(user.password, 8);
+        const hashPassword = AES.encrypt(user.password, process.env.JWT_SECRET).toString();
+        user.password = hashPassword;
+    }
     next();
 });
 
@@ -39,7 +43,13 @@ UserSchema.statics.findByCredentials = async function (email, password) {
         throw new Error('User not Found!')
     }
     // check if the password is valid
-    const isValidPass = await bcrypt.compare(password, user.password);
+    // const isValidPass = await bcrypt.compare(password, user.password);
+    const decryptPass =
+        AES.decrypt(user.password, process.env.JWT_SECRET)
+        .toString(enc.Utf8);
+
+    const isValidPass = decryptPass === password;
+
     console.log(isValidPass);
     if (!isValidPass) {
         throw new Error('Invalid Crendentials')
@@ -47,9 +57,9 @@ UserSchema.statics.findByCredentials = async function (email, password) {
     return user;
 }
 
-UserSchema.pre('remove', async function(next) {
+UserSchema.pre('remove', async function (next) {
     const user = this;
-    await Task.deleteMany({owner: user._id});
+    await Task.deleteMany({ owner: user._id });
     next();
 })
 
